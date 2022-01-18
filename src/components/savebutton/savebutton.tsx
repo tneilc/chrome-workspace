ca51b547ff1110221ff242beeefb9e38706b67d6
Metehan <65496622+tneilc@users.noreply.github.com>
2022-01-18 12:51:36 -0800

    savebutton.tsx

--- src/components/savebutton/savebutton.tsx
+++ src/components/savebutton/savebutton.tsx
@@ -1,1 +1,27 @@
+import { Component } from "react";
+import NameModal from "../namemodal/namemodal";
 import "./savebutton.css";
+
+class SaveButton extends Component<{}, { showModal: boolean }> {
+  constructor(props: any) {
+    super(props);
+    this.state = { showModal: false };
+  }
+
+  setModalState = () => {
+    this.setState({ showModal: !this.state.showModal });
+  };
+
+  render() {
+    return (
+      <div className="savebutton-wrapper">
+        <button onClick={this.setModalState} className="savebutton-btn">
+          Save Workspace
+        </button>
+        {this.state.showModal && <NameModal changeState={this.setModalState}/>}
+      </div>
+    );
+  }
+}
+
+export default SaveButton;
